42fb57cab3a82912117389a0bb6857d0ad3bf1ba
hekysc <heky.he@gmail.com>
2025-10-17 15:22:25 +0800

    Add image_files_count function to count images
    Added a function to count image files in a directory.

--- window_utils.py
+++ window_utils.py
@@ -1,4 +1,4 @@
-import win32gui
+import win32gui, os
 
 def get_window_list():
     windows = []
@@ -9,3 +9,20 @@ def get_window_list():
 
     win32gui.EnumWindows(callback, None)
     return windows
+
+def image_files_count(folder_path,image_type):
+    image_count = 0
+
+    # 检查是否存在这个路径
+    if not os.path.isdir(folder_path):
+        print("❌ 路径无效或不存在")
+        return 0
+
+    # 遍历当前文件夹中的文件（不进入子文件夹）
+    for file in os.listdir(folder_path):
+        if os.path.isfile(os.path.join(folder_path, file)):
+            _, ext = os.path.splitext(file.lower())
+            if ext in image_type:
+                image_count += 1
+
+    return image_count
